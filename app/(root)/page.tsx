9f9cd33526ab9dadbe6d702f071a17f75f372e4f
Kaleb-Besufikad <se.kaleb.besufikad@gmail.com>
2025-12-10 12:58:57 +0300

    setup Shadcn
    remove nextjs stock homepage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className={"flex justify-center items-center h-screen"}>
+<Button variant={"outline"}>Click mee</Button>
+    </div>
+  )
+}
+export default Home
